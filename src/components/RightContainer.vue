adf61e9df1d61d1c86275873fef1d1d2e1dc52f2
naybala <naybalaismylife.nbl@gmail.com>
2022-06-23 13:23:19 +0630

    commit

--- src/components/RightContainer.vue
+++ src/components/RightContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="right-Container">
-    <div class="profile-container mt-5 ms-2">
+    <div class="profile-container mt-3 ms-2">
       <!-- Education  Section Start-->
       <h3 class="fw-bold">Education</h3>
       <hr />
@@ -53,7 +53,74 @@
           </div>
         </h5>
       </div>
+      <br />
+      <br />
+      <br />
       <!-- Employment  Section End-->
+
+      <!-- Certificates  Section Start-->
+      <h3 class="fw-bold">Certificates And Degree</h3>
+      <hr />
+      <div>
+        <h5><span>Bachelor Of Technology (B.Tech)</span></h5>
+        <br />
+        <h5><span>Baechlor Of Engineering (B.E)</span></h5>
+        <br />
+        <h5><span>AIMT WEB Development</span></h5>
+        <br />
+      </div>
+      <br />
+      <br />
+      <br />
+      <!-- Certificates  Section End-->
+
+      <!-- Social Profile  Section Start-->
+      <h3 class="fw-bold">Social Profile</h3>
+      <hr />
+      <div>
+        <h5>
+          <span
+            ><a
+              href="https://www.facebook.com/profile.php?id=100076412994170"
+              target="_blink"
+              >Facebook</a
+            ></span
+          >
+        </h5>
+      </div>
+      <div>
+        <h5>
+          <span
+            ><a href="https://www.facebook.com/profile.php?id=100076412994170"
+              >Instagram</a
+            ></span
+          >
+        </h5>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <!-- Social Profile  Section End-->
+
+      <!-- Physical And Mental  Section Start-->
+      <h3 class="fw-bold">Physical And Mental</h3>
+      <hr />
+      <div>
+        <ul>
+          <li>awebsite wesign perspective.</li>
+          <li>I can work in accordance with Dealine.</li>
+          <li>I can work inateam and haveastrong desire to learn.</li>
+          <li>I can complete tasks within the specified time.</li>
+          <li>I have good teamwork and communication skills.</li>
+          <li>Self-directed and able to work without supervision.</li>
+          <li>Highly motivated and learn about new technology.</li>
+        </ul>
+      </div>
+      <br />
+      <br />
+      <br />
+      <!-- Physical And Mental  Section End-->
     </div>
   </div>
 </template>
@@ -62,10 +129,16 @@
 .right-Container {
   flex: 70%;
   background-color: rgb(208, 212, 207);
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
 }
 
 span {
   color: #569eac;
   font-weight: 700;
+}
+a {
+  text-decoration: none;
+  color: #569eac;
 }
 </style>
